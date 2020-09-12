a90140cf0f385a422b177ab4c06dcf0e15b494ef
ghozziayoub <ghozziayoub@gmail.com>
2020-09-12 16:07:46 +0100

    test mongo atlas 2

--- db/config.js
+++ db/config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://<ghozziayoub@gmail.com>:<azerty1234>@@cluster0.hsewk.gcp.mongodb.net/<erp-db>?retryWrites=true&w=majority"
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://testinguser:azerty147258369@cluster0.hsewk.gcp.mongodb.net/erp-db?retryWrites=true&w=majority"
 
 const MONGODB_OPTIONS = {
     useNewUrlParser: true,
